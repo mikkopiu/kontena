76f4ef37fa08724933c23271638965437c30653c
Mikko Piuhola <mikko.piuhola@gmail.com>
2017-10-09 22:08:04 +0300

    Middle: e2e tests

---
+++ test/spec/features/stack/stop_signal_spec.rb
@@ -0,0 +1,18 @@
+describe 'service link' do
+  after(:each) do
+    run "kontena stack rm --force stop_signal"
+  end
+
+  it 'stops container with user-defined signal' do
+    with_fixture_dir("stack/stop_signal") do
+      run 'kontena stack install'
+      sleep 1
+      id = container_id('stop_signal')
+
+      run 'kontena stack stop'
+      k = run "kontena container inspect #{id}"
+      "ExitCode": 137
+      expect(k.out.match(/^\s+\"ExitCode\"\:\s+0\,$/)).to be_truthy
+    end
+  end
+end
